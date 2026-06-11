f538f35b7e20ff00046b252e7477837dc9299014
ANCR Software <ma6996553@gmail.com>
2026-06-11 01:04:13 -0600

    Commit inicial: Configuración del proyecto Flutter

---
+++ ejercicio2_variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  final String pokemon = "ditto";
+  final int hp = 10;
+  final bool isAlive = true;
+  final List<String> abilities = ['impostor', 'amago'];
+  final habilidadesPokemon =  <String>['rodada', 'bola sombra'];
+  print("""
+      $pokemon
+      $hp
+    	$isAlive
+      $abilities
+      $habilidadesPokemon
+  """);
+}
