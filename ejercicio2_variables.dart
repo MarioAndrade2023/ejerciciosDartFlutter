b26fc80e6361fc48fd479147a21daf363b243537
ANCR Software <ma6996553@gmail.com>
2026-06-11 01:08:52 -0600

    prueba_subida de cambios

--- ejercicio2_variables.dart
+++ ejercicio2_variables.dart
@@ -2,8 +2,8 @@ void main() {
   final String pokemon = "ditto";
   final int hp = 10;
   final bool isAlive = true;
-  final List<String> abilities = ['impostor', 'amago'];
-  final habilidadesPokemon =  <String>['rodada', 'bola sombra'];
+  final List<String> abilities = ['impostor', 'amago', 'vuelo'];
+  final habilidadesPokemon =  <String>['rodada', 'bola sombra', 'hiperrayo'];
   print("""
       $pokemon
       $hp
